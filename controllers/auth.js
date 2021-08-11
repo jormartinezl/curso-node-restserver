e2fea11e9aaf4ed8bd4790a9c9a1892c18e1f4b4
jorgemartinez <www.jorge_a14@hotmail.com>
2021-08-11 12:55:10 -0500

    google sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const {response} = require('express');
+const {response, json} = require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs  = require('bcryptjs');
 const { generaJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req,res=response)=>{
 
@@ -48,6 +49,54 @@ const login = async(req,res=response)=>{
     }
 }
 
+const googleSignIn = async (req,res=response,next)=>{
+
+    
+    const {id_token} = req.body;
+    try {
+        const {name,email,image} = await googleVerify(id_token);
+
+        let usuario=await Usuario.findOne({email});
+
+        if(!usuario){
+            
+            const data ={
+                name,
+                email,
+                password: ':p',
+                image,
+                google: true,
+                rol: 'SUPER_ROLE'
+            }
+            
+            usuario = new Usuario(data);
+            await usuario.save();
+            console.log(usuario);
+        }
+        
+        if(!usuario.state){
+            return res.status(401).json({
+                msg: 'Hable con el administardor usuario borrado'
+            })
+        }
+        
+        //generar jwt
+        const token = await generaJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            msg: 'Token de google no reconocido'
+        })
+    }
+}
+
 module.exports={
-    login
+    login,
+    googleSignIn
 }
